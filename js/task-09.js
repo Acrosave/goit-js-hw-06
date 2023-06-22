69d1b065504fb0a124ba8196c1353ee99789c805
Santiago <santigomas20@gmail.com>
2023-06-22 15:04:30 -0500

    first pages common

--- js/task-09.js
+++ js/task-09.js
@@ -1,4 +1,3 @@
-document.querySelector('.color').textContent = getRandomHexColor();
 const bodyChange = document.querySelector('.change-color');
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -7,4 +6,5 @@ function getRandomHexColor() {
 }
 bodyChange.addEventListener('click', () => {
   document.body.style.backgroundColor = getRandomHexColor();
+  document.querySelector('.color').innerText = getRandomHexColor();
 });
